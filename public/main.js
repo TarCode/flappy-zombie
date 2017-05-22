bc701703ae6301f80053c00ceac8b198c718f7db
Taariq Isaacs <taariq@uber5.com>
2017-05-22 11:28:57 +0200

    added sprites for char and bg

--- public/main.js
+++ public/main.js
@@ -2,16 +2,19 @@
 const mainState = {
   preload: function () {
     // Load bird
-    game.load.image('bird', 'assets/bird.png')
-
+    game.load.image('bird', 'assets/png/male/dead12.png')
     // Load pipe sprite
     game.load.image('pipe', 'assets/pipe.png')
+
+    game.load.image('bg', 'assets/runners/desert_BG.png')
   },
 
   create: function () {
+    this.background = game.add.tileSprite(0, game.height - 400, game.width, 400, 'bg');
+
     this.pipes = game.add.group()
 
-    game.stage.backgroundColor = '#71c5cf'
+    game.stage.backgroundColor = '#71c'
 
     game.physics.startSystem(Phaser.Physics.ARCADE)
 
@@ -24,17 +27,33 @@ const mainState = {
     this.bird.body.gravity.y = 1000
 
     // add pipes every 1.5 seconds
-    this.timer = game.time.events.loop(1500 mthis.addRowOfPipes, this)
+    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this)
 
     const spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
 
     spaceKey.onDown.add(this.jump, this)
+
+    // Add and display score
+
+    this.score = 0
+
+    this.labelScore = game.add.text(20, 20, "0", {
+      font: "33px Verdana",
+      fill: "#fff"
+    })
   },
 
   update: function () {
+    // speed to scroll in pixels per second
+     var scrollSpeed = 50;
+
+     // a little bit of math to make sure the speed is consistent whatever the fps
+     this.background.tilePosition.x -= scrollSpeed * (game.time.elapsed * 0.001);
     // Restart game if bird is screwed
-    if (this.bird.y < 0 || this.bird.y > 490)
+    if (this.bird.y < 0 || this.bird.y > 400)
       this.restartGame()
+
+    game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this)
   },
 
   jump: function () {
@@ -66,10 +85,13 @@ const mainState = {
         this.addOnePipe(400, i * 60 + 10)
       }
     }
+
+    this.score += 1
+    this.labelScore.text = this.score
   }
 }
 
-var game = new Phaser.Game(400, 490)
+var game = new Phaser.Game(600, 400)
 
 game.state.add('main', mainState)
 
