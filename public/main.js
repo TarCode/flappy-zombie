9c63b1f513a706ffca7adcee15f498033758d82c
Taariq Isaacs <taariq@uber5.com>
2017-05-22 12:52:08 +0200

    made game canvas responsive

--- public/main.js
+++ public/main.js
@@ -1,12 +1,20 @@
 // Main state contains the game
 const mainState = {
   preload: function () {
-    // Load bird
-    game.load.image('bird', 'assets/png/male/dead12.png')
+    // Make game canvas responsive
+    game.scale.scaleMode = Phaser.ScaleManager.aspectRatio;
+    game.scale.pageAlignVertically = true;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.setShowAll();
+    game.scale.refresh();
+
+    // Load zombie
+    game.load.image('zombie', 'assets/png/male/dead12.png')
     // Load pipe sprite
     game.load.image('pipe', 'assets/pipe.png')
 
     game.load.image('bg', 'assets/runners/desert_BG.png')
+    game.load.image('bg2', 'assets/runners/swamp.png')
   },
 
   create: function () {
@@ -19,12 +27,12 @@ const mainState = {
     game.physics.startSystem(Phaser.Physics.ARCADE)
 
     // Display bnird at x: 100, y:245
-    this.bird = game.add.sprite(100, 245, 'bird')
-    //Add physics to bird
+    this.zombie = game.add.sprite(100, 245, 'zombie')
+    //Add physics to zombie
     //Needed for movements, gravity collisions, etc
-    game.physics.arcade.enable(this.bird)
+    game.physics.arcade.enable(this.zombie)
 
-    this.bird.body.gravity.y = 1000
+    this.zombie.body.gravity.y = 1000
 
     // add pipes every 1.5 seconds
     this.timer = game.time.events.loop(1500, this.addRowOfPipes, this)
@@ -37,7 +45,7 @@ const mainState = {
 
     this.score = 0
 
-    this.labelScore = game.add.text(20, 20, "0", {
+    this.labelScore = game.add.text(20, 20, "Score: 0", {
       font: "33px Verdana",
       fill: "#fff"
     })
@@ -49,15 +57,20 @@ const mainState = {
 
      // a little bit of math to make sure the speed is consistent whatever the fps
      this.background.tilePosition.x -= scrollSpeed * (game.time.elapsed * 0.001);
-    // Restart game if bird is screwed
-    if (this.bird.y < 0 || this.bird.y > 400)
+    // Restart game if zombie is screwed
+    if (this.zombie.y < 0 || this.zombie.y > 400)
       this.restartGame()
 
-    game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this)
+    if(this.score > 3)
+      this.background.loadTexture('bg2')
+
+    game.physics.arcade.overlap(this.zombie, this.pipes, this.restartGame, null, this)
+    game.scale.setShowAll();
+    game.scale.refresh();
   },
 
   jump: function () {
-    this.bird.body.velocity.y = -350
+    this.zombie.body.velocity.y = -350
   },
 
   restartGame: function () {
@@ -87,11 +100,11 @@ const mainState = {
     }
 
     this.score += 1
-    this.labelScore.text = this.score
+    this.labelScore.text = "Score: " + this.score
   }
 }
 
-var game = new Phaser.Game(600, 400)
+const game = new Phaser.Game(640, 400)
 
 game.state.add('main', mainState)
 
